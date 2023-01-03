157a2ed1016283f6c5bb3cf02f3a779723fc5a5e
Kanstancin Vienhura <vengurakonstant@gmail.com>
2023-01-03 20:47:25 +0100

    deploy fe with be

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const reviewHandlers = require("./reviewHandler");
 const cors = require("cors");
 const port = process.env.PORT ?? 5000;
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./UI/build")));
 
 const { validateReview } = require("./validators.js");
 
@@ -15,8 +17,8 @@ app.get("/api/reviews", reviewHandlers.getReviews);
 app.put("/api/reviews/:id", reviewHandlers.updateReview);
 app.delete("/api/reviews/:id", reviewHandlers.deleteReview);
 
-app.get("/", (_, res) => {
-  res.sendStatus(200);
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "./UI/build/index.html"));
 });
 
 app.listen(port, (err) => {
